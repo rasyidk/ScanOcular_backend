d9efe2f552e9841b9ef5a656230030cd44321dee
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-08-20 17:39:12 +0700

    chore : back to basic

--- scanocular_backend/wsgi.py
+++ scanocular_backend/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vercel_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scanocular_backend.settings")
 
 application = get_wsgi_application()
 app = application

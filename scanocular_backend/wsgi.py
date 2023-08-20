1949329f6c9cd7eb5a96756dd6108d0a53ceb24b
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-08-20 14:30:17 +0700

    feat : example and change config

--- scanocular_backend/wsgi.py
+++ scanocular_backend/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scanocular_backend.settings")
+
+# application = get_wsgi_application()
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scanocular_backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vercel_app.settings")
 
-application = get_wsgi_application()
+app = get_wsgi_application()

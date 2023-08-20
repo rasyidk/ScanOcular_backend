45e93a253cf38d31306e0093687e9b1329f63407
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-08-20 18:00:01 +0700

    chore : vercel app

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scanocular_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vercel_app.settings")
 
 app = get_wsgi_application()

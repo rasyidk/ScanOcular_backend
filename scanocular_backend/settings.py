8f03cdad47781509ee46afeac758d753a630acee
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-09-10 14:24:25 +0700

    feat : add allow headers

--- scanocular_backend/settings.py
+++ scanocular_backend/settings.py
@@ -85,6 +85,28 @@ TEMPLATES = [
 WSGI_APPLICATION = "scanocular_backend.wsgi.app"
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 CORS_ORIGIN_WHITELIST = (
     "http://example.com",
     "http://127.0.0.1:8000",

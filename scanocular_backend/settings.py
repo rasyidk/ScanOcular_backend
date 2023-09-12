d15bc32efb33ebab39d6f5e2855ff294e56af313
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-09-12 21:01:51 +0700

    feat : add screening all

--- scanocular_backend/settings.py
+++ scanocular_backend/settings.py
@@ -96,22 +96,22 @@ CORS_ALLOW_CREDENTIALS = False
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "u556939220_scanocular2",
-        "USER": "u556939220_scanocular2",
-        "PASSWORD": "Scanocular2",
-        "HOST": "trpl.space",  # Change this if your MySQL server is on a different host
-        "PORT": "3306",  # Change this if your MySQL server is using a different port
-    }
     # "default": {
     #     "ENGINE": "django.db.backends.mysql",
-    #     "NAME": "scanocular",
-    #     "USER": "root",
-    #     "PASSWORD": "123",
-    #     "HOST": "localhost",  # Change this if your MySQL server is on a different host
+    #     "NAME": "u556939220_scanocular2",
+    #     "USER": "u556939220_scanocular2",
+    #     "PASSWORD": "Scanocular2",
+    #     "HOST": "trpl.space",  # Change this if your MySQL server is on a different host
     #     "PORT": "3306",  # Change this if your MySQL server is using a different port
     # }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "scanocular",
+        "USER": "root",
+        "PASSWORD": "123",
+        "HOST": "localhost",  # Change this if your MySQL server is on a different host
+        "PORT": "3306",  # Change this if your MySQL server is using a different port
+    }
 }
 
 
